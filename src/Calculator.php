170484a63e32c28d3b5643675a48726df2e03289
Jhony Creativo <jhonycreativo.code@gmail.com>
2022-08-29 02:32:31 -0500

    first commit

--- src/Calculator.php
+++ src/Calculator.php
@@ -1,6 +1,6 @@
 <?php
 namespace Jhonycreativo\Mipaquete;
-
+use Illuminate\Support\Collection;
 class Calculator
 {
     public function sum($a, $b)
@@ -9,5 +9,21 @@ class Calculator
     }
     public function hola(){
         return "hola";
+    }
+    public function GroupAndSum($value = null,$param=null,$param2=null){
+        $data = collect($list);
+        $data = $data->groupBy($param)->map(function ($item) {
+            if ($item->count() > 1) {
+                $item = $item->slice(0, 1)->map(function($subItem) use ($item) {
+                    $subItem[$param2] = $item->sum($param2);
+
+                    return $subItem;
+                });
+            }
+
+            return $item;
+        })
+        ->flatten(1);
+        return $data;
     }
 }
